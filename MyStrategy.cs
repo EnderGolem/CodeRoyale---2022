c9292705a2a2524c7adc668a78887c11705fb940
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2022-07-08 19:33:38 +0300

    Debug info

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using System.Drawing;
+using AiCup22.Debugging;
 using AiCup22.Model;
+using Color = AiCup22.Debugging.Color;
 
 namespace AiCup22
 {
@@ -21,7 +24,36 @@ namespace AiCup22
 
         public void Analyze(Game game, DebugInterface debugInterface)
         {
-            
+            if (debugInterface != null)
+            {
+                Vec2 offset = new Vec2(-5, 5);
+                double textsize = 2;
+                Color textColor = new Color(0, 0, 1, 1);
+                DebugData.PlacedText text = new DebugData.PlacedText();
+                text.Text = "Hello world!";
+                Unit player = new Unit();
+                foreach (var unit in game.Units)
+                {
+                    if (unit.Id == debugInterface.GetState().LockedUnit)
+                    {
+                        player = unit;
+                    }
+                }
+
+                Vec2 debugTextPos = debugInterface.GetState().Camera.Center.Add(offset);
+                debugInterface.Add(new DebugData.PlacedText(debugTextPos, 
+                    $"Health: {player.Health}",
+                    new Vec2(0.5, 0.5), textsize, textColor));
+                debugInterface.Add(new DebugData.PlacedText(debugTextPos.Subtract(new Vec2(0,textsize/2)), 
+                    $"Shield: {player.Shield}",
+                    new Vec2(0.5, 0.5), textsize, textColor));
+                debugInterface.Add(new DebugData.PlacedText(debugTextPos.Subtract(new Vec2(0,2*textsize/2)), 
+                    $"Potions: {player.ShieldPotions}",
+                    new Vec2(0.5, 0.5), textsize, textColor));
+                debugInterface.Add(new DebugData.PlacedText(debugTextPos.Subtract(new Vec2(0, 3 * textsize / 2)),
+                    $"Velocity: {player.Velocity}",
+                    new Vec2(0.5, 0.5), textsize, textColor));
+            }
         }
     }
 
@@ -32,6 +64,7 @@ namespace AiCup22
 
     public class Brain: Processable
     {
+        private Processable currentState;
         /// <summary>
         /// Здесь дожен быть какой - то общий список всех конечных действий на выбор данного мозга
         /// </summary>
@@ -71,6 +104,7 @@ namespace AiCup22
 
                     continue;
                 }
+                
                 ActionOrder action = new ActionOrder.Aim(false);
                 System.Console.WriteLine(target.Position.SqrDistance(unit.Position));
                 UnitOrder order = new UnitOrder(
@@ -91,7 +125,11 @@ namespace AiCup22
             }
             return new Order(orders);
         }
-        public void DebugUpdate(DebugInterface debugInterface) { }
+
+        public void DebugUpdate(DebugInterface debugInterface)
+        {
+            
+        }
         public void Finish() { }
     }
     
