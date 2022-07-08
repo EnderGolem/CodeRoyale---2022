6831d56ab120293fea2d642034949852f462c501
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-07-08 19:16:08 +0300

    Create Basic
    Создана основа. Добавлены 4 действия.

--- Custom/EndAction.cs
+++ Custom/EndAction.cs
@@ -4,11 +4,55 @@ namespace AiCup22.Custom
 {
     public class EndAction
     {
-
-        public UnitOrder Process(Perception perception)
+        public UnitOrder Process(Perception perception, ref int id)
         {
-            return new UnitOrder();
+            throw new System.NotImplementedException();
         }
-
     }
+
+    public class RunToCenter : EndAction
+    {
+        public UnitOrder Process(Perception perception, int id)
+        {
+            ActionOrder action = new ActionOrder.Aim(false);
+            Unit unit = perception.MyUnints[0];
+            return new UnitOrder(new Vec2(-unit.Position.X, -unit.Position.Y),
+                new Vec2(-unit.Position.X, -unit.Position.Y), action);
+        }
+    }
+
+    public class RunShootToCenter : EndAction
+    {
+        public UnitOrder Process(Perception perception, int id)
+        {
+            ActionOrder action = new ActionOrder.Aim(false);
+            Unit unit = perception.MyUnints[0];
+            return new UnitOrder(new Vec2(-unit.Position.X, -unit.Position.Y),
+                new Vec2(-unit.Position.X, -unit.Position.Y), action);
+        }
+    }
+
+    public class RunToCenterRadar : EndAction
+    {
+        public UnitOrder Process(Perception perception, int id)
+        {
+            ActionOrder action = new ActionOrder.Aim(false);
+            Unit unit = perception.MyUnints[0];
+            return new UnitOrder(new Vec2(-unit.Position.X, -unit.Position.Y),
+               new Vec2(-unit.Direction.Y, unit.Direction.X), action);
+        }
+    }
+
+    public class RunShootToCenterRadar : EndAction
+    {
+        public UnitOrder Process(Perception perception, int id)
+        {
+            ActionOrder action = new ActionOrder.Aim(false);
+            Unit unit = perception.MyUnints[0];
+            return new UnitOrder(new Vec2(-unit.Position.X, -unit.Position.Y),
+               new Vec2(-unit.Direction.Y, unit.Direction.X), action);
+        }
+    }
+
+
 }
