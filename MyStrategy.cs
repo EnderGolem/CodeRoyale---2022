895fb6ed3d532de2e59116dafa7e5538bf3f486e
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-07-08 14:08:26 +0300

    Initial commit

---
+++ MyStrategy.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using AiCup22.Model;
+
+namespace AiCup22
+{
+    public class MyStrategy
+    {
+        int maxAttackDistance = 25;
+        public MyStrategy(AiCup22.Model.Constants constants) { System.Console.WriteLine(constants.ViewDistance); }
+
+        public AiCup22.Model.Order GetOrder(AiCup22.Model.Game game, DebugInterface debugInterface)
+        {
+            Dictionary<int, AiCup22.Model.UnitOrder> orders = new Dictionary<int, UnitOrder>();
+
+            Unit target = new Unit();
+            bool changed = false;
+            foreach (var unit in game.Units)
+            {
+                if (unit.PlayerId != game.MyId)
+                {
+                    changed = true;
+                    target = unit;
+
+                    continue;
+                }
+                ActionOrder action = new ActionOrder.Aim(false);
+                System.Console.WriteLine(target.Position.SqrDistance(unit.Position));
+                UnitOrder order = new UnitOrder(
+                    new Vec2(-unit.Position.X, -unit.Position.Y), new Vec2(-unit.Position.X, -unit.Position.Y), action);
+                if (changed && target.Position.SqrDistance(unit.Position) < maxAttackDistance * maxAttackDistance)
+                {
+                    Vec2 enemy = target.Position.Subtract(unit.Position);
+                    action = new ActionOrder.Aim(true);
+                    order = new UnitOrder(enemy, enemy, action);
+                }
+                if (changed && target.Position.SqrDistance(unit.Position) <= 13 * 13)
+                {
+                    Vec2 enemy = target.Position.Subtract(unit.Position);
+                    action = new ActionOrder.Aim(true);
+                    order = new UnitOrder(unit.Position, unit.Direction, action);
+                }
+                orders.Add(unit.Id, order);
+            }
+            return new Order(orders);
+        }
+        public void DebugUpdate(DebugInterface debugInterface) { }
+        public void Finish() { }
+    }
+    public static class Vec2Extensions
+    {
+        public static Vec2 Add(this Vec2 a, Vec2 b)
+        {
+            return new Vec2(a.X - b.X, a.Y - b.Y);
+        }
+        public static Vec2 Subtract(this Vec2 a, Vec2 b)
+        {
+            return new Vec2(a.X - b.X, a.Y - b.Y);
+        }
+        public static Vec2 Multi(this Vec2 a, Vec2 b)
+        {
+            return new Vec2(a.X - b.X, a.Y - b.Y);
+        }
+        public static double SqrDistance(this Vec2 a, Vec2 b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y);
+        }
+    }
+}
