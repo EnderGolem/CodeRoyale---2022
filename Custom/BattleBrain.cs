86b3a20c05f805dc43adeb938f7c92c4ca9a6da7
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-07-09 12:00:00 +0300

    Add basic BattleBrain.
    Добавлена основа для BattleBrain

---
+++ Custom/BattleBrain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AiCup22.Custom;
+using AiCup22.Model;
+
+namespace AiCup22.Custom
+{
+    class BattleBrain : Brain
+    {
+
+        ShootToPoint _shootToPoing;
+        AimingToPoint _aimingToPoint;
+        public BattleBrain()
+        {
+            _shootToPoing = new ShootToPoint();
+            _aimingToPoint = new AimingToPoint();
+        }
+        public override UnitOrder Process(Perception perception)
+        {
+            _aimingToPoint.SetTarget(perception.MyUnints[id].Direction);
+            return _aimingToPoint.Process(perception, id);
+        }
+    }
+}
