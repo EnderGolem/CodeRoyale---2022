0f1e31b434809af5f97bb08eb123a00a856ed91a
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-07-09 11:52:31 +0300

    add Protected int id.
    В brain добавленн id, чтобы не забыли о нем.

--- Custom/LootingBrain.cs
+++ Custom/LootingBrain.cs
@@ -39,32 +39,32 @@ namespace AiCup22.Custom
                 }
             }
 
-            Console.WriteLine(perception.Game.Loot[bestLootIndex].Position.Distance(perception.MyUnints[0].Position));
-            if (perception.Game.Loot[bestLootIndex].Position.Distance(perception.MyUnints[0].Position) <
+            Console.WriteLine(perception.Game.Loot[bestLootIndex].Position.Distance(perception.MyUnints[id].Position));
+            if (perception.Game.Loot[bestLootIndex].Position.Distance(perception.MyUnints[id].Position) <
                 perception.Constants.UnitRadius / 2)
             {
                 Console.WriteLine("Start pickup!!!");
                 _pickupLoot.SetPickableLootId(perception.Game.Loot[bestLootIndex].Id);
-                return _pickupLoot.Process(perception, 0);
+                return _pickupLoot.Process(perception, id);
             }
             else
             {
                 _runToDestination.SetDestination(perception.Game.Loot[bestLootIndex].Position);
-                return _runToDestination.Process(perception, 0);
+                return _runToDestination.Process(perception, id);
             }
         }
 
         private double CalculateLootValue(Perception perception, Loot loot)
         {
-            double points = -loot.Position.SqrDistance(perception.MyUnints[0].Position);
+            double points = -loot.Position.SqrDistance(perception.MyUnints[id].Position);
             switch (loot.Item)
             {
                 case Item.Weapon weapon:
                     ///Можно также рассмотреть ситуацию когда нет патронов для нашего оружия
                     /// Но рядом есть патроны для другого
-                    if ((perception.MyUnints[0].Weapon.HasValue) &&
+                    if ((perception.MyUnints[id].Weapon.HasValue) &&
                         (weapon.TypeIndex == perception.MyUnints[0].Weapon.Value) ||
-                        (perception.MyUnints[0].Weapon.Value == 2))
+                        (perception.MyUnints[id].Weapon.Value == 2))
                     {
                         points -= 10000000;
                     }
@@ -86,8 +86,8 @@ namespace AiCup22.Custom
 
                     break;
                 case Item.Ammo ammo:
-                    if (perception.MyUnints[0].Weapon.HasValue &&
-                        ammo.WeaponTypeIndex == perception.MyUnints[0].Weapon.Value)
+                    if (perception.MyUnints[id].Weapon.HasValue &&
+                        ammo.WeaponTypeIndex == perception.MyUnints[id].Weapon.Value)
                     {
                         ///Надо написать формулу очков в зависимости от кол-ва патронов и других
                         /// факторов
