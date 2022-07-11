b06b0586f97306748fc361050237831e83e63d06
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-07-11 11:56:07 +0300

    Fix before Update
    Исправлен MyStrategy, Runner, исправлена версия Net.
    Исправлен Баг из-за чего все не работало.

--- MyStrategy.cs
+++ MyStrategy.cs
@@ -1,11 +1,29 @@
+using System.Collections.Generic;
+using AiCup22.Model;
+using AiCup22.Custom;
+
+
 namespace AiCup22
 {
+
     public class MyStrategy
     {
-        public MyStrategy(AiCup22.Model.Constants constants) {}
+        private Perception perception;
+        private Brain brain;
+
+        public MyStrategy(AiCup22.Model.Constants constants)
+        {
+            perception = new Perception(constants);
+            brain = new GeneralBrain();
+        }
+
         public AiCup22.Model.Order GetOrder(AiCup22.Model.Game game, DebugInterface debugInterface)
         {
-            return new AiCup22.Model.Order(new System.Collections.Generic.Dictionary<int, AiCup22.Model.UnitOrder>());
+
+            perception.Analyze(game, debugInterface);
+            Dictionary<int, AiCup22.Model.UnitOrder> orders = new Dictionary<int, UnitOrder>();
+            orders.Add(perception.MyUnints[0].Id, brain.Process(perception, debugInterface));
+            return new Order(orders);
         }
         public void DebugUpdate(int displayedTick, DebugInterface debugInterface) {}
         public void Finish() {}
