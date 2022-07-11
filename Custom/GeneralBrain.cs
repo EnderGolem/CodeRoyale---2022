68a3e80694f935f33b04152acb068b7ca381d6c7
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-07-11 11:42:22 +0300

    Add Koefficent

--- Custom/GeneralBrain.cs
+++ Custom/GeneralBrain.cs
@@ -6,10 +6,10 @@ namespace AiCup22.Custom
 {
     public class GeneralBrain : Brain
     {
-        protected const double healthValueKoefBattle = 1;
-        protected const double shieldValueKoefBattle = 1;
-        protected const double maxAmmoValueConst = 100;
-        protected const double maxPotionsValueLoot = 100;
+        protected const double healthValueBattle = Koefficient.healthValueBattle;
+        protected const double shieldValueBattle = Koefficient.shieldValuefBattle;
+        protected const double maxAmmoValue = Koefficient.maxAmmoValue;
+        protected const double maxPotionsValueLoot = Koefficient.maxPotionsValueLoot;
         
         private LootingBrain _lootingBrain;
         private BattleBrain _battleBrain;
@@ -97,10 +97,11 @@ namespace AiCup22.Custom
             }
             else
             {
-                value += (unit.Weapon.Value * 50) + maxAmmoValueConst * unit.Ammo[unit.Weapon.Value]/perception.Constants.Weapons[unit.Weapon.Value].MaxInventoryAmmo;
+                value += (unit.Weapon.Value * 50) + maxAmmoValue * 
+                    unit.Ammo[unit.Weapon.Value]/perception.Constants.Weapons[unit.Weapon.Value].MaxInventoryAmmo;
             }
 
-            value += healthValueKoefBattle * unit.Health + shieldValueKoefBattle * unit.Shield;
+            value += healthValueBattle * unit.Health + shieldValueBattle * unit.Shield;
 
             value -= 150;
 
@@ -111,7 +112,7 @@ namespace AiCup22.Custom
         {
             Unit unit = perception.MyUnints[0];
             double value = 0;
-            if (!unit.Weapon.HasValue)
+            if (!unit.Weapon.HasValue) //???
             {
                 value += 100;
             }
@@ -119,13 +120,13 @@ namespace AiCup22.Custom
             {
                 
                 double weaponValue = unit.Weapon.Value * 100;
-                double ammoValue = maxAmmoValueConst *
+                double ammoValue = maxAmmoValue *
                                    ((double) (unit.Ammo[unit.Weapon.Value]) / perception.Constants
                                        .Weapons[unit.Weapon.Value].MaxInventoryAmmo); 
-                Console.WriteLine($"Weapon value: {unit.Weapon.Value * 50} AmmoValue: {ammoValue}");
                 double potionsValue = maxPotionsValueLoot * ((double)unit.ShieldPotions /
                                       perception.Constants.MaxShieldPotionsInInventory);
                 value += (450 - weaponValue - ammoValue - potionsValue);
+                Console.WriteLine($"Weapon value: {unit.Weapon.Value * 50} AmmoValue: {ammoValue}");
             }
 
             //value += healthValueKoefBattle * unit.Health + shieldValueKoefBattle * unit.Shield;
