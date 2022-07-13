0f08cad7ee660c07991dbbba656af72db189a6bf
EnderGolem <83020228+EnderGolem@users.noreply.github.com>
2022-07-13 21:06:13 +0300

    Fix Runner
    Из раннера удален лишний мусор.
    Добавлен класс виртуальных нажатий.
    Добавлен класс для множественного тестирования.

---
+++ Custom/VirtualInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace AiCup22.Custom
+{
+    public static class VirtualInnput
+    {
+        private enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+        private const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
+        private const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
+        private const int VK_RCONTROL = 0x1B; //Right Control key code
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        public static void SetCursorClick(int x, int y)
+        {
+
+            Thread.Sleep(100);
+            SetCursorPos(x, y);
+            Thread.Sleep(100);
+            mouse_event((int)(MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            Thread.Sleep(100);
+            mouse_event((int)(MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+        }
+        public static void PressEsk()
+        {
+            Thread.Sleep(500);
+            keybd_event(VK_RCONTROL, 0, KEYEVENTF_EXTENDEDKEY, 0);
+            //Thread.Sleep(500);
+            //keybd_event(VK_RCONTROL, 0, KEYEVENTF_KEYUP, 0);
+        }
+    }
+}
